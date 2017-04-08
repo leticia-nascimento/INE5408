66fce5f50cfc9bdf6dc251190010698e7776b254
Leticia <leticianascimento.cco@gmail.com>
2017-04-07 23:29:30 -0300

    Funcionando <3

--- linked_list.h
+++ linked_list.h
@@ -9,289 +9,245 @@
 
 namespace structures {
 
-    /**
-     * @brief      Implementação de uma Estrutura de Dados do tipo Lista Encadeada
-     *			   Organização de dados linear e dinâmica, composta por células que
-     *             apontam para o próximo elemento da lista.
-     *
-     * @tparam     T     Parâmetro genérico de Template
-     */
+/**
+ * @brief      Implementação de uma Estrutura de Dados do tipo Lista Encadeada
+ *			   Organização de dados linear e dinâmica, composta por células que
+ *             apontam para o próximo elemento da lista.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
+template<typename T>
+class LinkedList {
+ public:
+    LinkedList();  // construtor padrão
+    ~LinkedList();  // destrutor
+    void clear();  // limpar lista
+    void push_back(const T& data);  // inserir no fim
+    void push_front(const T& data);  // inserir no início
+    void insert(const T& data, std::size_t index);   // inserir na posição
+    void insert_sorted(const T& data);  // inserir em ordem
+    T& at(std::size_t index);  // acessar um elemento na posição index
+    T pop(std::size_t index);  // retirar da posição
+    T pop_back();  // retirar do fim
+    T pop_front();  // retirar do início
+    void remove(const T& data);  // remover específico
+    bool empty() const;  // lista vazia
+    bool contains(const T& data) const;  // contém
+    std::size_t find(const T& data) const;  // posição do dado
+    std::size_t size() const;  // tamanho da lista
 
-    template<typename T>
-    class LinkedList {
-    public:
-        LinkedList(); // construtor padrão 
-        ~LinkedList();  // destrutor
-        void clear();  // limpar lista
-        void push_back(const T& data);  // inserir no fim
-        void push_front(const T& data);  // inserir no início
-        void insert(const T& data, std::size_t index);   // inserir na posição
-        void insert_sorted(const T& data);  // inserir em ordem
-        T& at(std::size_t index);  // acessar um elemento na posição index
-        T pop(std::size_t index);  // retirar da posição
-        T pop_back();  // retirar do fim
-        T pop_front();  // retirar do início
-        void remove(const T& data);  // remover específico
-        bool empty() const;  // lista vazia
-        bool contains(const T& data) const;  // contém
-        std::size_t find(const T& data) const;  // posição do dado
-        std::size_t size() const;  // tamanho da lista
-    private:
-        class Node { // Elemento
-        public:
-            Node(const T& data):
-                data_{data}
-            {}
-    
-            Node(const T& data, Node* next):
-                data_{data},
-                next_{next}
-            {}
-    
-            T& data() { // getter: dado
-                return data_;
-            }
+ private:
+    class Node {  // Elemento
+     public:
+        explicit Node(const T& data) : data_{data}{}
+        Node(const T& data, Node* next) : data_{data},next_{next}{}
+        T& data() { return data_; }  // getter: dado
+        const T& data() const { return data_; }  // getter const: dado
+        Node* next() { return next_; }  // getter: próximo
+        const Node* next() const { return next_; }  // getter const: próximo 
+        void next(Node* node) { next_ = node; }  // setter: próximo
 
-            const T& data() const { // getter const: dado
-                return data_;
-            }
+     private:
+        T data_;
+        Node* next_{nullptr};
+    };
 
-            Node* next() { // getter: próximo
-                return next_;
-            }
-
-            const Node* next() const { // getter const: próximo
-                return next_;
-            }
-
-            void next(Node* node) { // setter: próximo
-                next_ = node;
-            }
-        private:
-            T data_;
-            Node* next_{nullptr};
-        };
-
-        Node* end() { // último nodo da lista
-            auto it = head;
-            for (auto i = 1u; i < size(); ++i) {
-                it = it->next();
-            }
-            return it;
+    // último nodo da lista
+    Node* end() {
+        auto it = head;
+        for (auto i = 1u; i < size(); ++i) {
+            it = it->next();
         }
+        return it;
+    }
 
-        void delete_contents();
-
-        Node* head{nullptr};
-        std::size_t size_{0u};
+    Node* head{nullptr};
+    std::size_t size_{0u};
 };
 
-}  // namespace structures
-
-//  construtor
-
+// Construtor
 template <typename T>
-structures::LinkedList<T>::LinkedList():
-    head{nullptr},
-    size_{0u}
-{}
+LinkedList<T>::LinkedList() {
+    head = nullptr;
+    size_ = 0;
+}
 
-
-//  destrutor
-
+// Destrutor
 template<typename T>
-structures::LinkedList<T>::~LinkedList() {
-    auto current = head;
-    while (current != nullptr) {
-        auto prev = current;
-        current = current -> next();
-        delete prev;
-    }
+LinkedList<T>::~LinkedList() {
+    clear();
 }
 
 //  limpa lista
-
 template<typename T>
-void structures::LinkedList<T>::clear() {
-    auto current = head;
-    while (current != nullptr) {
-        auto prev = current;
-        current = current -> next();
-        delete prev;
+void LinkedList<T>::clear() {
+    while(!empty()) {
+        pop_front();
     }
-    head = nullptr;
-    size_ = 0u;
 }
 
 //  verifica se a lista está vazia
-
 template<typename T>
-bool structures::LinkedList<T>::empty() const {
-    return (size_ == 0);
+bool LinkedList<T>::empty() const {
+    return size_ == 0;
 }
 
 //  retorna o tamanho da lista
-
 template<typename T>
-std::size_t structures::LinkedList<T>::size() const {
+std::size_t LinkedList<T>::size() const {
     return size_;
 }
 
 //  inserir no fim
-
 template<typename T>
-void structures::LinkedList<T>::push_back(const T& data) {
-    insert(data, size_ );
+void LinkedList<T>::push_back(const T& data) {
+    insert(data, size_);
 }
 
 // insere numa posição específica
-
 template<typename T>
-void structures::LinkedList<T>::insert(const T& data, std::size_t index) {
-    if (index < 0)
+void LinkedList<T>::insert(const T& data, std::size_t index) {
+    if (index < 0 || index > size())
         throw std::out_of_range("Índice inválido");
         
     if (index == 0)
         return push_front(data);
-        
-    std::size_t i = 0;
+    
+    Node* novo = new Node(data);
+    if (novo == nullptr)
+        throw std::out_of_range("Lista cheia.");
+    
     Node* atual = head;
-    while (i < index - 1) {
-        i++;
-        atual = atual -> next();
+    for (int i = 0; i < index - 1; i++) {
+      atual = atual->next();
     }
-   Node *novo = new Node(data,atual->next());
-   atual -> next(novo);
-   size_++;
+    novo->next(atual->next());
+    atual->next(novo);
+    size_++;
 }
 
 //  insere no começo
-
 template<typename T>
-void structures::LinkedList<T>::push_front(const T& data) {
-    insert(data, 0);
+void LinkedList<T>::push_front(const T& data) {
+    Node* first_node = new Node(data);
+    if (first_node == nullptr)
+        throw std::out_of_range("Lista está cheia.");
+
+    first_node->next(head);
+    head = first_node;
+    size_++;
 }
 
 //  insere em ordem
-
 template<typename T>
-void structures::LinkedList<T>::insert_sorted(const T& data) {
-    if (empty())
+void LinkedList<T>::insert_sorted(const T& data) {
+    if (empty()){
         push_front(data);
-
-    std::size_t i = 0;
-    Node* current = head;
-    while (i < size_ && current->data() < data) {
-        i++;
-        current = current->next();
+    } else {
+        std::size_t i = 0;
+        Node* current = head;
+        for (i = 0; i < size_ && data > current->data(); i++) {
+          current = current->next();
+        }
+        insert(data, i);
     }
-   insert(data, i);
 }
 
 //  acessar um elemento pelo índice
-
 template<typename T>
-T& structures::LinkedList<T>::at(std::size_t index) {
+T& LinkedList<T>::at(std::size_t index) {
     Node* current = head;
     if (index > size_ - 1)
         throw std::out_of_range("Índice inválido!");
-    if (empty())
-        throw std::out_of_range("Lista vazia!");
+    
     if (index == 0)
         return current->data();
-    std::size_t i = 0;
-    while (i < index) {
-        i++;
-        current = current->next();
+    
+    for (int i = 0; i < index; i++) {
+      current = current->next();
     }
-return current->data();
+    return current->data();
 }
 
 //  retira um elemento no indice
-
 template<typename T>
-T structures::LinkedList<T>::pop(std::size_t index) {
-    if (index > size_)
-        throw std::out_of_range("Índice inválido!");
+T LinkedList<T>::pop(std::size_t index) {
     if (empty())
         throw std::out_of_range("Lista vazia!");
 
+    if (index > (size_ - 1))
+        throw std::out_of_range("Índice inválido!");
+
+    if (index == 0) 
+        return pop_front();
+
     std::size_t i = 0;
     Node* anterior = head;
-    Node* atual;
-    T retorno;
-    while (i < index - 1) {
-        i++;
-        anterior = anterior->next();
+    for (i = 0; i < index - 1; i++) {
+      anterior = anterior->next();
     }
-    atual = anterior->next();
-    retorno = atual->data();
+    Node* atual = anterior->next();
+    T retorno = atual->data();
     anterior->next(atual->next());
-    delete(atual);
     size_--;
-return retorno;
+    delete atual;
+    return retorno;
 }
 
 //  retira o ultimo elemento
-
 template<typename T>
-T structures::LinkedList<T>::pop_back() {
-    return pop(size_);
+T LinkedList<T>::pop_back() {
+    return pop(size() - 1);
 }
 
 //  retira o primeiro elemento
-
 template<typename T>
-T structures::LinkedList<T>::pop_front() {
+T LinkedList<T>::pop_front() {
+    if (empty()) {
+        throw std::out_of_range("Lista está vazia!");
+    }
     Node* anterior = head;
     T out_ = anterior->data();
     head = anterior->next();
-    delete(anterior);
+    delete anterior;
     size_--;
     return out_;
 }
 
 // remove um item específico
-
 template<typename T>
-void structures::LinkedList<T>::remove(const T& data) {
+void LinkedList<T>::remove(const T& data) {
     std::size_t test = find(data);
-    if (test != size())
+    if (test != size_)
         pop(test);
 }
 
 //  verifica se a contém um dado específico
-
 template<typename T>
-bool structures::LinkedList<T>::contains(const T& data) const {
-    std::size_t i = 0;
-    Node* atual = head;
-    while (atual->data() != data) {
-        i++;
-        atual = atual -> next();
-    }
-    if (atual != nullptr)
-        return 1;
-    return 0;
+bool LinkedList<T>::contains(const T& data) const {
+    if (find(data) != size_)
+        return true;
+    return false;
+    // return find(data) != size_;
 }
 
 //  retorna a posição de um dado específico
-
 template<typename T>
-std::size_t structures::LinkedList<T>::find(const T& data) const {
-    if (empty())
-        throw std::out_of_range("Lista vazia!");
+std::size_t LinkedList<T>::find(const T& data) const {
+    std::size_t i;
+    std::size_t index = size_;
+    Node* current = head;
 
-    if (!contains(data))
-        throw std::out_of_range("Elemento inexistente.");
-
-    std::size_t i = 0;
-    Node* atual = head;
-    while (atual->data() != data) {
-        i++;
-        atual = atual -> next();
+    for (i = 0; i < size_; i++) {
+        if (current->data() == data) {
+            index = i;
+            break;
+        }
+        if (i != size_)
+            current = current->next();
     }
-    return i;
+    return index;
 }
 
+}  // namespace
 #endif
---
+++ linked_list_correct.h
@@ -0,0 +1,280 @@
+// Copyright 2017 Eduardo Kohler
+
+#ifndef STRUCTURES_ARRAY_LIST_H
+#define STRUCTURES_ARRAY_LIST_H
+
+#include <cstdint>    // std::size_t
+#include <stdexcept>  // C++ Exceptions
+
+namespace structures {
+/**
+ * Implementation of a template linked list.
+ */
+template <typename T>
+class LinkedList {
+ public:
+  LinkedList();
+  ~LinkedList();
+  void clear();
+  void push_back(const T& data);
+  void push_front(const T& data);
+  void insert(const T& data, std::size_t index);
+  void insert_sorted(const T& data);
+  T& at(std::size_t index);
+  T pop(std::size_t index);
+  T pop_back();
+  T pop_front();
+  void remove(const T& data);
+  bool empty() const;
+  bool contains(const T& data) const;
+  std::size_t find(const T& data) const;
+  std::size_t size() const;
+
+ private:
+  class Node {
+   public:
+    explicit Node(const T& data) : data_{data} {}
+
+    Node(const T& data, Node* next) : data_{data}, next_{next} {}
+
+    T& data() { return data_; }
+
+    const T& data() const { return data_; }
+
+    Node* next() { return next_; }
+
+    const Node* next() const { return next_; }
+
+    void next(Node* node) { next_ = node; }
+
+   private:
+    T data_;
+    Node* next_{nullptr};
+  };
+
+  Node* end() {
+    auto it = head;
+    for (auto i = 1u; i < size(); ++i) {
+      it = it->next();
+    }
+    return it;
+  }
+  Node* head{nullptr};
+  std::size_t size_{0u};
+};
+/**
+ * Constructor. Sets the attributes values.
+ */
+template <typename T>
+LinkedList<T>::LinkedList() {
+  head = nullptr;
+  size_ = 0;
+}
+/**
+ * Destructor. Deletes the contents[] array.
+ */
+template <typename T>
+LinkedList<T>::~LinkedList() {
+  clear();
+}
+/**
+ * Clears the list.
+ */
+template <typename T>
+void LinkedList<T>::clear() {
+  while (!empty()) {
+    pop_front();
+  }
+}
+/**
+ * Checks if the list is empty.
+ * \return boolean
+ */
+template <typename T>
+bool LinkedList<T>::empty() const {
+  return size_ == 0;
+}
+/**
+ * Adds an element to the front of the list.
+ */
+template <typename T>
+void LinkedList<T>::push_front(const T& data) {
+  
+  // Node* first = new Node(data);
+  // if (first == nullptr) {
+  //   throw std::out_of_range("Lista cheia.");
+  // }
+  // first->next(head);
+  // head = first;
+  // size_++;
+}
+/**
+ * Removes the first element.
+ * \return The first element of type T.
+ */
+template <typename T>
+T LinkedList<T>::pop_front() {
+  if (empty()) {
+    throw std::out_of_range("Lista vazia");
+  } else {
+    Node* first = head;
+    T out_ = first->data();
+    head = first->next();
+    delete first;
+    size_--;
+    return out_;
+  }
+}
+/**
+ * Inserts an element in the specified location.
+ * \param data data reference;
+ * \param index index where the element will be put in.
+ */
+template <typename T>
+void LinkedList<T>::insert(const T& data, std::size_t index) {
+  Node* input;
+  Node* prev;
+  if (index > size()) {
+    throw std::out_of_range("Index inválido");
+  } else if (index == 0) {
+    push_front(data);
+  } else {
+    input = new Node(data);
+    if (input == nullptr) {
+      throw std::out_of_range("Lista cheia.");
+    }
+    prev = head;
+    for (int i = 0; i < index - 1; i++) {
+      prev = prev->next();
+    }
+    input->next(prev->next());
+    prev->next(input);
+    size_++;
+  }
+}
+/**
+ * Takes an element out by its index.
+ * \param index element index.
+ */
+template <typename T>
+T LinkedList<T>::pop(std::size_t index) {
+  T output_;
+  Node* popped;
+  Node* prev;
+  if (empty()) {
+    throw std::out_of_range("Lista vazia");
+  } else if (index > size() - 1) {
+    throw std::out_of_range("Parâmetro inválido");
+  } else if (index == 0) {
+    return pop_front();
+  } else {
+    prev = head;
+    for (int i = 0; i < index - 1; i++) {
+      prev = prev->next();
+    }
+    popped = prev->next();
+    output_ = popped->data();
+    prev->next(popped->next());
+    size_--;
+    delete popped;
+    return output_;
+  }
+}
+/**
+ * Inserts an element respecting a specific progression.
+ * \param data data.
+ */
+template <typename T>
+void LinkedList<T>::insert_sorted(const T& data) {
+  Node* current;
+  if (empty()) {
+    push_front(data);
+  } else {
+    current = head;
+    std::size_t pos;
+    for (pos = 0; pos < size() && data > current->data(); pos++) {
+      current = current->next();
+    }
+    insert(data, pos);
+  }
+}
+/**
+ * Adds an element to the end of the list.
+ */
+template <typename T>
+void LinkedList<T>::push_back(const T& data) {
+  insert(data, size());
+}
+/**
+ * Removes the last element.
+ */
+template <typename T>
+T LinkedList<T>::pop_back() {
+  return pop(size() - 1);
+}
+/**
+ * Returns a reference to the element at the specified index.
+ */
+template <typename T>
+T& LinkedList<T>::at(std::size_t index) {
+  Node* current;
+  if (index > size() - 1) {
+    throw std::out_of_range("Index inválido");
+  } else if (index == 0) {
+    current = head;
+    return current->data();
+  } else {
+    current = head;
+    for (int i = 0; i < index; i++) {
+      current = current->next();
+    }
+    return current->data();
+  }
+}
+/**
+ * Checks if the list has a certain element, and if so, returns its index.
+ * \return specified element index.
+ */
+template <typename T>
+std::size_t LinkedList<T>::find(const T& data) const {
+  Node* current = head;
+  std::size_t index = size();
+  for (std::size_t i = 0; i < size(); i++) {
+    if (current->data() == data) {
+      index = i;
+      break;
+    }
+    if (i != size()) {
+      current = current->next();
+    }
+  }
+  return index;
+}
+/**
+ * Removes an element by its content.
+ * \param data data.
+ */
+template <typename T>
+void LinkedList<T>::remove(const T& data) {
+  std::size_t test = find(data);
+  if (test != size()) {
+    pop(test);
+  }
+}
+/**
+ * Checks if the list contains a certain element by its content.
+ * \return boolean
+ */
+template <typename T>
+bool LinkedList<T>::contains(const T& data) const {
+  return find(data) != size();
+}
+/**
+ * Returns the list current size.
+ */
+template <typename T>
+std::size_t LinkedList<T>::size() const {
+  return size_;
+}
+}  // namespace structures
+#endif
